71f1dc2b50cad29ff2efa5a61d65483566727f47
benlipkin <bglipkin@gmail.com>
2022-11-21 15:40:46 -0500

    log summary results

--- probsem/probsem.py
+++ probsem/probsem.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from probsem.abstract import Object
 from probsem.benchmarks import Prompt, TestSuite
 from probsem.models import Model
-from probsem.utils import normalize, pretty_print
+from probsem.utils import normalize, pretty_print_sample, pretty_print_summary
 
 
 class ProbSem(Object):
@@ -33,7 +33,18 @@ class ProbSem(Object):
             np.array([sample["pos_weight"], sample["neg_weight"]])
         )
 
+    @staticmethod
+    def _summarize(
+        samples: typing.List[typing.Dict[str, typing.Any]]
+    ) -> typing.Tuple[np.float64, np.float64]:
+        pos_weights = np.array([sample["pos_weight"] for sample in samples])
+        neg_weights = np.array([sample["neg_weight"] for sample in samples])
+        mean_accuracy = np.mean(pos_weights > neg_weights)
+        mean_score, _ = normalize(np.array([pos_weights.mean(), neg_weights.mean()]))
+        return mean_accuracy, mean_score
+
     def run(self) -> None:
+        samples = []
         for sample in tqdm(self._samples, total=self._suite.n_examples):
             sample["pos_weight"] = self._model.score(
                 sample["pos_full"], sample["pos_eval"]
@@ -42,4 +53,6 @@ class ProbSem(Object):
                 sample["neg_full"], sample["neg_eval"]
             )
             self._normalize_weights(sample)
-            self.info(pretty_print(sample))
+            self.info(pretty_print_sample(sample))
+            samples.append(sample)
+        self.info(pretty_print_summary(*self._summarize(samples)))
--- probsem/utils.py
+++ probsem/utils.py
@@ -22,11 +22,18 @@ def normalize(weights: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     return np.exp(weights) / np.sum(np.exp(weights))
 
 
-def pretty_print(sample: typing.Dict[str, typing.Any]) -> str:
+def pretty_print_sample(sample: typing.Dict[str, typing.Any]) -> str:
     ostream = []
     ostream.append("Text:")
     ostream.append(f"{sample['text']}")
     ostream.append("Scores:")
     ostream.append(f"{sample['pos_score']:.3f}\t{sample['pos_eval']}")
     ostream.append(f"{sample['neg_score']:.3f}\t{sample['neg_eval']}")
+    return "\n".join([30 * "_"] + ostream + [30 * "_"])
+
+
+def pretty_print_summary(mean_accuracy: np.float64, mean_score: np.float64) -> str:
+    ostream = []
+    ostream.append(f"TEST SUITE ACCURACY:\t{mean_accuracy:.3f}")
+    ostream.append(f"TEST SUITE SCORE:\t{mean_score:.3f}")
     return "\n".join([30 * "_"] + ostream + [30 * "_"])
